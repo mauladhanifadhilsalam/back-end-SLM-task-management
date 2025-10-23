6c92645e471e807e27c9e795577cc0ec93a0655f
vathmos <esqeue@gmail.com>
2025-10-23 13:11:44 +0700

    remove example endpoints

--- src/app.ts
+++ src/app.ts
@@ -38,40 +38,4 @@ app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "Welcome to SLM Project Management API" });
 });
 
-// Admin role authorization example
-app.get(
-  "/admin-dashboard",
-  requireRole(RoleType.ADMIN),
-  (_req: Request, res: Response) => {
-    res.json({ message: "Welcome to Admin Dashboard" });
-  },
-);
-
-// Project Manager role authorization example
-app.get(
-  "/pm-dashboard",
-  requireRole(RoleType.PROJECT_MANAGER),
-  (_req: Request, res: Response) => {
-    res.json({ message: "Welcome to PM Dashboard" });
-  },
-);
-
-// Developer role authorization example
-app.get(
-  "/developer-dashboard",
-  requireRole(RoleType.DEVELOPER),
-  (_req: Request, res: Response) => {
-    res.json({ message: "Welcome to Developer Dashboard" });
-  },
-);
-
-// multiple role authorization example
-app.get(
-  "/projects",
-  requireRole([RoleType.ADMIN, RoleType.PROJECT_MANAGER]),
-  (_req: Request, res: Response) => {
-    res.json({ projects: ["Project 1", "Project 2", "Project 3"] });
-  },
-);
-
 export default app;
