34cfc4b4523b5d059136a252bcd03827882836c5
vathmos <esqeue@gmail.com>
2025-11-12 20:22:53 +0700

    add base url environment variable

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,9 +4,11 @@ dotenv.config();
 const env: {
   jwtSecret: string;
   jwtExpiresIn: string | number;
+  baseUrl: string;
 } = {
   jwtSecret: process.env.JWT_SECRET || "secret",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || 3600,
+  baseUrl: process.env.BASE_URL || "http://localhost:3000",
 };
 
 export default env;
