f4254cbef61f2c5b8450cc9a3f57904ec77cbfd1
vathmos <esqeue@gmail.com>
2025-11-14 09:53:52 +0700

    remove unused environment variable BASE_URL

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,11 +4,9 @@ dotenv.config();
 const env: {
   jwtSecret: string;
   jwtExpiresIn: string | number;
-  baseUrl: string;
 } = {
   jwtSecret: process.env.JWT_SECRET || "secret",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || 3600,
-  baseUrl: process.env.BASE_URL || "http://localhost:3000",
 };
 
 export default env;
