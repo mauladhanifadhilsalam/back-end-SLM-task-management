f0fef6a949af56af3ea4a2f965b3aa5f1da22f4f
vathmos <esqeue@gmail.com>
2025-11-28 10:18:35 +0700

    add new endpoint for project manager dashboard

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -1,6 +1,6 @@
 import prisma from "../db/prisma";
 
-export type DeveloperDashboard = [
+type DeveloperDashboard = [
   {
     userId: number;
     fullName: string;
@@ -38,14 +38,23 @@ export type DeveloperDashboard = [
   }
 ];
 
+type ProjectManagerDashboard = [
+  {}
+]
 
-async function findDeveloperDashboard(
-  userId: number,
-): Promise<DeveloperDashboard> {
+
+async function findDeveloperDashboard(userId: number): Promise<DeveloperDashboard> {
   return await prisma.$queryRaw`SELECT * FROM developer_dashboard WHERE "userId" = ${userId} LIMIT 1`;
 }
 
-async function refreshDashboard() {
-  return await prisma.$executeRaw`REFRESH MATERIALIZED VIEW developer_dashboard`;
+async function findProjectManagerDashboard(userId: number): Promise<ProjectManagerDashboard> {
+  return await prisma.$queryRaw`SELECT * FROM project_manager_dashboard WHERE "userId" = ${userId} LIMIT 1`;
 }
-export { findDeveloperDashboard, refreshDashboard };
+
+async function refreshDashboard() {
+  return await prisma.$transaction([
+  prisma.$executeRaw`REFRESH MATERIALIZED VIEW developer_dashboard`,
+  prisma.$executeRaw`REFRESH MATERIALIZED VIEW project_manager_dashboard`
+  ])
+}
+export { findDeveloperDashboard, findProjectManagerDashboard, refreshDashboard };
