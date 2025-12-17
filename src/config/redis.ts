dca359ed2f0d7ce8f63856b039db452142bce010
Ahmad Faris Widyawan <esqeue@gmail.com>
2025-12-17 14:20:32 +0700

    restructure env example and extend redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,7 +2,11 @@ import IORedis from 'ioredis';
 import env from './env';
 
 export const connection = new IORedis({
+  tls: env.redisTLS,
+  username: env.redisUsername,
+  password: env.redisPassword,
   host: env.redisHost,
   port: env.redisPort,
+  db: env.redisDB,
   maxRetriesPerRequest: null
 });
