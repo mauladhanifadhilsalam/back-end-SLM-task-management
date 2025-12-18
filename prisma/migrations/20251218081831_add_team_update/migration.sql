4e8afc614b59022a13f6d63210a70bc315e1cf4e
Ahmad Faris Widyawan <esqeue@gmail.com>
2025-12-18 15:53:45 +0700

    add team update schema

---
+++ prisma/migrations/20251218081831_add_team_update/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "TeamUpdate" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "yesterdayWork" TEXT NOT NULL,
+    "todayWork" TEXT NOT NULL,
+    "blocker" TEXT NOT NULL,
+    "nextAction" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TeamUpdate_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "TeamUpdate" ADD CONSTRAINT "TeamUpdate_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
